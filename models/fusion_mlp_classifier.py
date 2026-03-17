56a3cc0e5d50cbacfda47efdfc50b5dcd2ef01a6
Emanuele Rucci <emanuele.rucci.work@gmail.com>
2026-03-17 16:49:10 +0100

    Fixed seed and classification

--- models/fusion_mlp_classifier.py
+++ models/fusion_mlp_classifier.py
@@ -33,3 +33,29 @@ class LinearProbing(nn.Module):
     def forward(self, text_emb, vision_emb):
         x = torch.cat([text_emb, vision_emb], dim=-1)  # (B, 1024)
         return self.vision_proj(x)  # simple late fusion
+    
+class LinearProbingIndependentModalities(nn.Module):
+    def __init__(self, d=512, num_classes=1000):
+        super().__init__()
+        self.embedding_projected = nn.Linear(d, num_classes)
+
+    def forward(self, embedding):
+        return self.embedding_projected(embedding)  # (B, num_classes)
+    
+class NonLinearProbingIndependentModalities(nn.Module):
+    def __init__(self, d=512, num_classes=1000, hidden=1024, dropout=0.2):
+        super().__init__()
+        self.net = nn.Sequential(
+            nn.Linear(d, hidden),
+            nn.GELU(),
+            nn.LayerNorm(hidden),
+            nn.Dropout(dropout),
+            nn.Linear(hidden, hidden // 2),
+            nn.GELU(),
+            nn.LayerNorm(hidden // 2),
+            nn.Dropout(dropout),
+            nn.Linear(hidden // 2, num_classes),
+        )
+
+    def forward(self, embedding):
+        return self.net(embedding)  # (B, num_classes)
